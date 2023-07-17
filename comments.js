a47e5329efae644f42b037bd85142d431bf8e313
Suryansh Chauhan <72202812+Suryansh-23@users.noreply.github.com>
2023-07-17 22:27:36 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,45 +1,74 @@
-// Create a web server
-var express = require("express");
-var router = express.Router();
+// Create web server
+// Load modules
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const mysql = require('mysql');
+const config = require('../config/config.json');
 
-// Import the model (comment.js) to use its database functions.
-var comment = require("../models/comment.js");
+// MySQL
+const db = mysql.createConnection({
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database
+});
 
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function (req, res) {
-    comment.all(function (data) {
-        var hbsObject = {
-            comments: data,
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
+// Connect to database
+db.connect((err) => {
+    if(err) throw err;
+    console.log('MySQL connected');
+});
+
+// Get all comments
+router.get('/', (req, res) => {
+    let sql = 'SELECT * FROM comments';
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        res.json(results);
     });
 });
 
-router.post("/api/comments", function (req, res) {
-    comment.create(["comment"], [req.body.comment], function (result) {
-        // Send back the ID of the new quote
-        res.json({ id: result.insertId });
+// Get a single comment
+router.get('/:id', (req, res) => {
+    let sql = `SELECT * FROM comments WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.json(result);
     });
 });
 
-router.put("/api/comments/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
-
-    comment.update(
-        {
-            comment: req.body.comment,
-        },
-        condition,
-        function (result) {
-            if (result.changedRows == 0) {
-                // If no rows were changed, then the comment does not exist, so 404
-                return res.status(404).end();
-            } else {
-                res.status(200).end();
-            }
-        }
-    );
+// Insert a comment
+router.post('/', [
+    check('name').isLength({ min: 2 }),
+    check('comment').isLength({ min: 2 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    };
+    let sql = 'INSERT INTO comments SET ?';
+    let query = db.query(sql, req.body, (err, result) => {
+        if(err) throw err;
+        res.send('Comment added');
+    });
 });
+
+// Update a comment
+router.put('/:id', [
+    check('name').isLength({ min: 2 }),
+    check('comment').isLength({ min: 2 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    };
+    let sql = `UPDATE comments SET name = '${req.body.name}', comment = '${req.body.comment}' WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send('Comment updated');
+    });
+});
+
+// Delete a comment
+router.delete('/:id',
